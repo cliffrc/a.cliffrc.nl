57ee6136a7fce561590c5e3ac5a93b24345c47ab
Cliff Cunnington <cliff@10strata.com>
2023-12-06 17:01:09 +0100

    Merged old content with new layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { HeartPulse } from "lucide-react";
 
 const Contact = () => {
   return (
     <section className="scroll-mt-[4.7rem] " id="contact">
       <div>Contact</div>
+      <HeartPulse className="mt-16 -mb-1 h-12 w-12 text-zinc-600/50 shadow-3xl  dark:text-white/60" />
+      <p>FIXME up with a form please</p>
     </section>
   );
 };
