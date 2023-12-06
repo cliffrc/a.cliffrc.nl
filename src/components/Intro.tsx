f9ba3084ade1e381be920cef4346a301d9d8f395
Cliff Cunnington <cliff@10strata.com>
2023-12-06 17:09:46 +0100

    Merged old content with new layout

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,6 +4,9 @@ const Intro = () => {
   return (
     <section className="sm:scroll-mt-[4.7rem] " id="home">
       <div>Intro</div>
+
+      <h2 className="dark:text-white/70">Live long and prosper</h2>
+      <span className="text-4xl">🖖</span>
     </section>
   );
 };
