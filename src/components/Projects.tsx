fc24a3bba2bff4ed2f569ef515405991f2da9087
Cliff Cunnington <cliff@10strata.com>
2023-12-06 17:11:07 +0100

    Merged old content with new layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,16 @@
+import { Clapperboard } from "lucide-react";
 import React from "react";
 
 const Projects = () => {
   return (
     <section className="scroll-mt-[4.7rem] " id="projects">
+      <Clapperboard className="mt-16 -mb-1 h-12 w-12 text-zinc-600/50 shadow-3xl dark:text-white/60" />
       <div>Projects</div>
+      <ul>
+        <li>10strata, with PapyInChat</li>
+        <li>We Style Stuff</li>
+        <li>Luiard Press</li>
+      </ul>
     </section>
   );
 };
