7e3993739f3fa4ea1b767c370a5dbf45729a6d6b
Cliff Cunnington <cliff@10strata.com>
2023-12-06 17:18:13 +0100

    Merged old data with new data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -24,3 +24,39 @@ export const navLinks = [
     hash: "#contact",
   },
 ] as const;
+
+export const techSkillsData = [
+  "Next.js",
+  "TypeScript",
+  "TailwindCSS",
+  "tRPC",
+  "Prisma",
+  "Planetscale",
+  "Pinecone",
+  "Vercel",
+  "OpenAI",
+  "HTML",
+  "SQL",
+  "Git",
+  "Linux / FreeBSD",
+  "Python",
+  "R",
+  "JavaScript",
+  "HTMX",
+] as const;
+
+export const businessSkillsData = [
+  "Social Media Marketing",
+  "Content Repurposing",
+  "Content Marketing",
+  "Growth Hacking",
+  "Ideation (ChatGPT)",
+] as const;
+
+export const personalSkillsData = [
+  "goal-driven",
+  "creative",
+  "tenacious",
+  "persistent",
+  "resilient",
+] as const;
