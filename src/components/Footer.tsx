c68e717f1c1dd9e45f512ad53e6f6900bc17fa65
Cliff Cunnington <cliff@10strata.com>
2023-12-06 13:26:39 +0100

    Complete Navbar, Footer and Primary Components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
+import { metadata } from "@/app/layout";
 import React from "react";
+import lightFormat from "date-fns/lightFormat";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; {lightFormat(new Date(), "yyyy")} {metadata.creator}. All
+        rights reserved.
+      </small>
+      <p className="text-xs">
+        built with TailwindCSS, Next.js (App Router and Server Actions),
+        TypeScript, Vercel hosting
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { navLinks } from "@/lib/data";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
   return (
     <header className="z-[999] relative">
-      <div className="w-full  h-[4.44rem] fixed top-0 rounded-none border  border-black/40 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] left-1/2 -translate-x-1/2 sm:rounded-full"></div>
-      <nav className="flex fixed top-[0.19rem]">
-        <ul>
+      <div className="w-full  h-[4.44rem] fixed top-0 rounded-none border  border-black/40 bg-white/50 backdrop-blur-sm sm:top-6 sm:h-[3.25rem] sm:w-[36rem] left-1/2 -translate-x-1/2 sm:rounded-full"></div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 -translate-x-1/2 h-12 py-2  sm:top-[1.7rem] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-1 font-medium text-sm text-zinc-500/90 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {navLinks.map((navlink) => (
-            <li key={navlink.hash}>{navlink.name}</li>
+            <li
+              key={navlink.hash}
+              className="h-3/4 flex items-center justify-center relative
+            "
+            >
+              <Link
+                className={cn(
+                  "flex w-full items-center justify-centerp-3 hover:text-zinc-950 transition dark:text-zinc-500 dark:hover:gray-300"
+                )}
+                href={navlink.hash}
+              >
+                {navlink.name}
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
