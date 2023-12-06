0918dd51c726cd80b576e94f2a5278a2d1ef79ff
Cliff Cunnington <cliff@10strata.com>
2023-12-06 13:50:30 +0100

    Minor change to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Projects from "@/components/Projects";
@@ -10,12 +8,12 @@ import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <div>
-      <MaxWidthWrapper className="mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <MaxWidthWrapper className="mt-12 sm:mt-40 flex flex-col items-center justify-center text-center">
         <Intro />
         <About />
         <Skills />
+
         <Projects />
-        <Experience />
         <Contact />
       </MaxWidthWrapper>
     </div>
