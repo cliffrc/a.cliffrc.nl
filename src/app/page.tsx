1e8e90bb6ecd3dab1fe3cb17d46ec5e8c0c73add
Cliff Cunnington <cliff@10strata.com>
2023-12-06 16:19:50 +0100

    Update root page.tsx with Experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Projects from "@/components/Projects";
@@ -8,11 +9,11 @@ import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <div>
-      <MaxWidthWrapper className="mt-12 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <MaxWidthWrapper className="mt-12 sm:mt-24 mb-4 flex flex-col items-center justify-center text-center">
         <Intro />
         <About />
         <Skills />
-
+        <Experience />
         <Projects />
         <Contact />
       </MaxWidthWrapper>
